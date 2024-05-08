51d62ddbc18f7c7c72a52f05d07e82afcfbf3272
AI_xplore <98645710+AIxploreRCC@users.noreply.github.com>
2024-05-08 15:24:43 -0500

    source.py

---
+++ Source.py
@@ -0,0 +1,14 @@
+import streamlit as st
+import pandas as pd
+import dill
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Fonction pour charger le modèle sauvegardé
+@st.cache(allow_output_mutation=True)
+def load_model():
+    with open('model_cox.pkl', 'rb') as f:
+        model = dill.load(f)
+    return model
+
+model_cox = load_model()
