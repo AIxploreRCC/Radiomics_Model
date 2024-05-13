9fe05a95e67e76c93e0cc162ffa23da10878bbc9
AI_xplore <98645710+AIxploreRCC@users.noreply.github.com>
2024-05-12 20:18:41 -0500

    Update Source.py

--- Source.py
+++ Source.py
@@ -5,37 +5,37 @@ import numpy as np
 import matplotlib.pyplot as plt
 import nibabel as nib
 
-
-
+# Decorator to cache model loading
 @st.cache(allow_output_mutation=True)
 def load_model():
     with open('model_cox.pkl', 'rb') as f:
         model = dill.load(f)
     return model
 
-
-# Chargement du modèle
+# Load the Cox model
 model_cox = load_model()
 
-# Titre de l'application Streamlit
+# Title of the Streamlit app
 st.title("Survival Prediction with the Cox Model")
 
-# Entrées pour les variables du modèle
+# Inputs for the model's variables
 hb = st.slider("Hemoglobin Level", min_value=0.0, max_value=20.0, value=10.0, step=0.1)
-kn564 = st.selectbox("KEYNOTE-564 inclusion criteria", options=[0, 1], format_func=lambda x: "high risk" if x == 1 else "intermediate-high")
+kn564 = st.selectbox("KEYNOTE-564 inclusion criteria", options=[0, 1], format_func=lambda x: "High risk" if x == 1 else "Intermediate-high")
 rad_score = st.slider("Radiomics Signature", min_value=0.0, max_value=1.0, value=0.5, step=0.1)
 
+# DataFrame for model input
 input_df = pd.DataFrame({
     'Hb': [hb],
     'KN564': [kn564],
     'RAD_Score': [rad_score]
 })
 
-# Bouton pour prédire la survie
+# Button to predict survival
 if st.button('Predict Survival'):
-    try:
-        survival_function = model_cox.predict_survival_function(input_df)
-        st.subheader('Estimated Survival Probability:')
-        st.line_chart(survival_function)
-    except Exception as e:
-        st.error(f"Prediction failed: {e}")
+    with st.spinner('Calculating... Please wait.'):
+        try:
+            survival_function = model_cox.predict_survival_function(input_df)
+            st.subheader('Estimated Survival Probability:')
+            st.line_chart(survival_function)
+        except Exception as e:
+            st.error(f"Prediction failed: {e}")
